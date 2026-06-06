d076e203533be7d7234d12e40269d52cf3a408d6
sppustovoitenko2-netizen <sppustovoitenko2@gmail.com>
2026-06-06 11:05:29 +0300

    Add main.py with basic functions

---
+++ main.py
@@ -0,0 +1,12 @@
+def greet(name):
+    return f'Hello, {name}!'
+
+def add(a, b):
+    return a + b
+
+def main():
+    print(greet('World'))
+    print(add(3, 5))
+
+if __name__ == '__main__':
+    main()
